2a702a5994a7a86969ab33798eb29c4e57b2ecab
kim <kim@pizslacker.org>
2026-09-14 14:43:43 +0200

    force padded bytes, to show static size increase.

--- math_lib.c
+++ math_lib.c
@@ -1,5 +1,8 @@
 #include "math_lib.h"
 
+// A massive 5MB dummy array embedded in the library
+const char massive_dummy_data[5000000] = {1}; 
+
 long long sum_array(const long long *arr, size_t size) {
     long long sum = 0;
     for (size_t i = 0; i < size; i++) {
